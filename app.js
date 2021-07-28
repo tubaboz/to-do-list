455a911e927a5973b07a1951b367a13eff1b9604
tubasaf <77580305+tubasaf@users.noreply.github.com>
2021-07-28 23:24:23 +0200

    commit1

---
+++ app.js
@@ -0,0 +1,6 @@
+//selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
